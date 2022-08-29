4dd8eafb4517c129ae948e7ac19c23bb0b0af454
Stanislav Stoyanov <grimerssy@gmail.com>
2022-08-29 10:59:09 +0300

    add zoxide

--- main.zsh
+++ main.zsh
@@ -1,12 +1,12 @@
-ZDOTDIR=$HOME/.config/zsh
-HISTFILE=$HOME/.zsh_history
+ZDOTDIR="$HOME/.config/zsh"
+HISTFILE="$HOME/.zsh_history"
 HISTSIZE=1000000
 SAVEHIST=750000
 setopt appendhistory
 
 setopt autocd extendedglob nomatch menucomplete
 setopt interactive_comments
-stty stop undef		# Disable ctrl-s to freeze terminal.
+stty stop undef # Disable ctrl-s to freeze terminal.
 zle_highlight=("paste:none")
 
 unsetopt BEEP
@@ -14,7 +14,7 @@ unsetopt BEEP
 autoload -Uz compinit
 zstyle ":completion:*" menu select
 zmodload zsh/complist
-_comp_options+=(globdots)		# Include hidden files.
+_comp_options+=(globdots) # Include hidden files.
 
 autoload -U up-line-or-beginning-search
 autoload -U down-line-or-beginning-search
@@ -26,3 +26,5 @@ autoload -Uz colors && colors
 zsh_add_plugin "zsh-users/zsh-autosuggestions"
 zsh_add_plugin "zsh-users/zsh-syntax-highlighting"
 zsh_add_plugin "hlissner/zsh-autopair"
+
+eval "$(zoxide init zsh)"
