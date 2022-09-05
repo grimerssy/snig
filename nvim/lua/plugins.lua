050138f0d9ce1b5405564c2f21da7655a99132c6
Stanislav Stoyanov <grimerssy@gmail.com>
2022-09-05 21:08:50 +0300

    (nvim): add packer auto-install

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,3 +1,17 @@
+local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+---@diagnostic disable-next-line: missing-parameter
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = vim.fn.system({
+    'git',
+    'clone',
+    '--depth',
+    '1',
+    'https://github.com/wbthomason/packer.nvim',
+    install_path,
+  })
+  vim.cmd([[packadd packer.nvim]])
+end
+
 local packer = require('packer')
 
 packer.init({
