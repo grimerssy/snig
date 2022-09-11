7df28edca9ac66f1cbd0ba6163393f17d378ec22
Stanislav Stoyanov <grimerssy@gmail.com>
2022-09-11 20:39:33 +0300

    (bin): add .sh extensions to scripts

--- bin/tmux-cht.sh
+++ bin/tmux-cht.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-selected=`cat ~/.local/bin/tmux-cht-sh-lists/languages ~/.local/bin/tmux-cht-sh-lists/commands | fzf --reverse`
+selected=`cat ~/.local/bin/tmux-cht-lists/languages ~/.local/bin/tmux-cht-lists/commands | fzf --reverse`
 
 if [[ -z $selected ]]; then
     exit 0
 fi
 
-if grep -qs "$selected" ~/.local/bin/tmux-cht-sh-lists/languages; then
+if grep -qs "$selected" ~/.local/bin/tmux-cht-lists/languages; then
     read -p "Enter Query: " query
     query=`echo $query | tr ' ' '+'`
     tmux neww sh -c "curl -s cht.sh/$selected/$query | less -r"
