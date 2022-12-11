f68b401cf481df0d0ddf27a90c95a5783569343b
Stanislav Stoyanov <grimerssy@gmail.com>
2022-12-11 17:42:25 +0200

    (zsh): remove redundant vim keybinds

--- zsh/vim.zsh
+++ zsh/vim.zsh
@@ -1,13 +1,6 @@
 bindkey -v
 export KEYTIMEOUT=1
 
-bindkey -M menuselect "^h" vi-backward-char
-bindkey -M menuselect "^k" vi-up-line-or-history
-bindkey -M menuselect "^l" vi-forward-char
-bindkey -M menuselect "^j" vi-down-line-or-history
-bindkey -M menuselect "^[[Z" vi-up-line-or-history
-bindkey -v "^?" backward-delete-char
-
 function zle-keymap-select () {
     case $KEYMAP in
         vicmd) echo -ne "\e[1 q";;      # block
