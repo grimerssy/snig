e8a1bd67831c46c74877e5cecc1c42d2c0f6211d
Stanislav Stoyanov <grimerssy@gmail.com>
2022-12-26 21:13:59 +0200

    (nvim): add treesitter injections for sql in rust

---
+++ nvim/after/queries/rust/injections.scm
@@ -0,0 +1,21 @@
+; extends
+
+(macro_invocation
+ (scoped_identifier
+    path: (identifier) @_path (#eq? @_path "sqlx")
+    name: (identifier) @_name (#any-of? @_name "query" "query_as")
+)
+ (token_tree
+   (raw_string_literal) @sql
+  )
+)
+
+(call_expression
+  function: (scoped_identifier
+    path: (identifier) @_path (#eq? @_path "sqlx")
+    name: (identifier) @_name (#any-of? @_name "query" "query_as")
+  )
+  arguments: (arguments
+     (raw_string_literal) @sql
+  )
+)
