a1271223229ba4052d9952e49dd0882b2e0e680a
Stanislav Stoyanov <grimerssy@gmail.com>
2023-01-02 17:41:09 +0200

    (zsh): remove outdated aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,14 +1,9 @@
-alias g="source go-version-manager.sh"
 alias n="nvim"
 alias l="lazygit"
 alias t="tmux"
 alias a="tmux attach"
 alias s="tmux-session.sh"
 alias c="clear"
-
-if [[ $TERM == "xterm-kitty" ]]; then
-  alias ssh="kitty +kitten ssh"
-fi
 
 case "$(uname -s)" in
 Darwin)
