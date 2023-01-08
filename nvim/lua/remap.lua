02fee93a46b1f397258db7769f67694f909134d2
Stanislav Stoyanov <grimerssy@gmail.com>
2023-01-08 19:19:50 +0200

    (nvim): updated remaps

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -4,7 +4,6 @@ local v = require('keymap').vnoremap
 local x = require('keymap').xnoremap
 
 n('<leader>so', '<CMD>luafile %<CR>')
-n('<leader>sy', '<CMD>PackerSync<CR>')
 n('<C-r', '<CMD>redo<CR>')
 n('<C-h>', '<C-w>h')
 n('<C-j>', '<C-w>j')
@@ -12,6 +11,13 @@ n('<C-k>', '<C-w>k')
 n('<C-l>', '<C-w>l')
 n('<C-d>', '<C-d>zz')
 n('<C-u>', '<C-u>zz')
+n('n', 'nzzzv')
+n('N', 'Nzzzv')
+n('<C-j>', 'cnext<CR>zz')
+n('<C-k>', 'cprev<CR>zz')
+n('<leader>n', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>')
+-- to work with lsp remaps
+n('<leader>rn', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>')
 
 i('jk', '<ESC>')
 
