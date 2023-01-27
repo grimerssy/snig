733e947cdc7cbbd3fc91791cba12fac06c92a69f
Stanislav Stoyanov <grimerssy@gmail.com>
2023-01-27 14:55:59 +0200

    (nvim): remove selecting with tab from cmp

--- nvim/lua/configs/cmp.lua
+++ nvim/lua/configs/cmp.lua
@@ -56,9 +56,7 @@ cmp.setup({
     }),
     ['<CR>'] = cmp.mapping.confirm({ select = false }),
     ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expandable() then
+      if luasnip.expandable() then
         luasnip.expand()
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
@@ -72,9 +70,7 @@ cmp.setup({
       's',
     }),
     ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
+      if luasnip.jumpable(-1) then
         luasnip.jump(-1)
       else
         fallback()
