01344c569496a05303429f700184aa4f8da5d831
Stanislav Stoyanov <grimerssy@gmail.com>
2023-01-27 21:23:05 +0200

    (nvim): update remaps

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -4,19 +4,15 @@ local v = require('keymap').vnoremap
 local x = require('keymap').xnoremap
 
 n('<leader>so', '<CMD>luafile %<CR>')
-n('<C-r', '<CMD>redo<CR>')
-n('<C-h>', '<C-w>h')
-n('<C-j>', '<C-w>j')
-n('<C-k>', '<C-w>k')
-n('<C-l>', '<C-w>l')
-n('<C-d>', '<C-d>zz')
-n('<C-u>', '<C-u>zz')
+n('<C-r>', '<CMD>redo<CR>')
+n('<C-d>', '<C-d>zzzv')
+n('<C-u>', '<C-u>zzzv')
 n('n', 'nzzzv')
 n('N', 'Nzzzv')
 n('<C-j>', 'cnext<CR>zz')
 n('<C-k>', 'cprev<CR>zz')
 n('<leader>n', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>')
--- to work with lsp remaps
+-- to not overlap with lsp remaps
 n('<leader>rn', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>')
 n('<leader>x', '<CMD>!chmod +x %<CR>')
 
