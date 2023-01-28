dd3149bb0d85df482be52face5312373b6e969d8
Stanislav Stoyanov <grimerssy@gmail.com>
2023-01-28 12:27:10 +0200

    (bin): apply bashls fixes

--- bin/tmux-cht.sh
+++ bin/tmux-cht.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-selected=`cat ~/.local/bin/tmux-cht-lists/languages ~/.local/bin/tmux-cht-lists/commands | fzf --reverse`
+selected=$(cat ~/.local/bin/tmux-cht-lists/languages ~/.local/bin/tmux-cht-lists/commands | fzf --reverse)
 
 if [[ -z $selected ]]; then
     exit 0
@@ -8,7 +8,7 @@ fi
 
 if grep -qs "$selected" ~/.local/bin/tmux-cht-lists/languages; then
     read -p "Enter Query: " query
-    query=`echo $query | tr ' ' '+'`
+    query=$(echo "$query" | tr ' ' '+')
     tmux neww sh -c "curl -s cht.sh/$selected/$query | less -r"
 else
     tmux neww sh -c "curl -s cht.sh/$selected | less -r"
--- bin/tmux-session.sh
+++ bin/tmux-session.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ZOXIDE_RESULT=$(zoxide query $1)
+ZOXIDE_RESULT=$(zoxide query "$1")
 
 if [ $# -eq 0 ] || [ -z "$ZOXIDE_RESULT" ]; then
   ZOXIDE_RESULT=$(zoxide query -l | fzf --reverse)
@@ -10,24 +10,24 @@ if [ -z "$ZOXIDE_RESULT" ]; then
   exit 0
 fi
 
-FOLDER=$(basename $ZOXIDE_RESULT | tr '.' '_')
+FOLDER=$(basename "$ZOXIDE_RESULT" | tr '.' '_')
 
-SESSION=$(tmux list-sessions | grep $FOLDER | awk '{print $1}')
+SESSION=$(tmux list-sessions | grep "$FOLDER" | awk '{print $1}')
 SESSION=${SESSION//:/}
 
 if [ -z "$SESSION" ]; then
-  cd $ZOXIDE_RESULT
-  tmux new-session -d -s $FOLDER
+  cd "$ZOXIDE_RESULT" || exit
+  tmux new-session -d -s "$FOLDER"
   tmux send-keys -t "$FOLDER:1" "v ." Enter
 fi
 
 if [ -z "$TMUX" ]; then
-  tmux attach -t $SESSION
+  tmux attach -t "$SESSION"
   exit 0;
 fi
 
 if [ -z "$SESSION" ]; then
-  tmux switch-client -t $FOLDER
+  tmux switch-client -t "$FOLDER"
 else
-  tmux switch-client -t $SESSION
+  tmux switch-client -t "$SESSION"
 fi
