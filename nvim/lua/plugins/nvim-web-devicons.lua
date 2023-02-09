ee3d4f79019ba882d1459b8c706837a7ffb755e2
Stanislav Stoyanov <grimerssy@gmail.com>
2023-02-09 20:11:21 +0200

    (nvim): fix cjs and mjs icons

--- nvim/lua/plugins/nvim-web-devicons.lua
+++ nvim/lua/plugins/nvim-web-devicons.lua
@@ -3,11 +3,23 @@ return {
   config = function()
     require('nvim-web-devicons').setup({
       override = {
-        astro = {
+        ['astro'] = {
           icon = '',
           color = '#ff5d01',
           cterm_color = '202',
           name = 'Astro',
+        },
+        ['mjs'] = {
+          icon = '',
+          color = '#cbcb41',
+          cterm_color = '185',
+          name = 'Js',
+        },
+        ['cjs'] = {
+          icon = '',
+          color = '#cbcb41',
+          cterm_color = '185',
+          name = 'Js',
         },
       },
     })
