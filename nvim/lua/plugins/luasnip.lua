df1ec93e1198c63e89b71217c3f7a1f1d00748e3
Stanislav Stoyanov <grimerssy@gmail.com>
2023-02-17 12:49:01 +0200

    (nvim): update snippets

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -10,23 +10,25 @@ return {
     local i = ls.insert_node
 
     ls.add_snippets('rust', {
+      s('tests', {
+        t({ '#[cfg(test)]', 'mod tests {', '    ' }),
+        i(1),
+        t({ '', '}' }),
+      }),
       s('tokio-main', {
-        t('#[tokio::main]'),
-        t({ '', 'async fn main() {', '    ' }),
+        t({ '#[tokio::main]', 'async fn main()', '    ' }),
         i(1, 'unimplemented!();'),
         t({ '', '}' }),
       }),
       s('tokio-test', {
-        t('#[tokio::test]'),
-        t({ '', 'async fn ' }),
+        t({ '#[tokio::test]', 'async fn ' }),
         i(1, 'it_works'),
         t({ '() {', '    ' }),
         i(2, 'unimplemented!();'),
         t({ '', '}' }),
       }),
       s('sqlx-test', {
-        t('#[sqlx::test]'),
-        t({ '', 'async fn ' }),
+        t({ '#[sqlx::test]', 'async fn ' }),
         i(1, 'it_works'),
         t('('),
         i(2, 'param: Type'),
