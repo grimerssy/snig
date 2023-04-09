566d3a4b71f1e14ca9d936b019ee747711f18fb7
grimerssy <grimerssy@gmail.com>
2023-04-09 12:59:04 +0300

    use same shasum in tmux-session and direnv cache

--- home/programs/direnv.nix
+++ home/programs/direnv.nix
@@ -1,15 +1,19 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
     enableNushellIntegration = false;
-    stdlib = ''
+    stdlib = let
+      cut = "${pkgs.coreutils}/bin/cut";
+      sha256sum = "${pkgs.coreutils}/bin/sha256sum";
+      basename = "${pkgs.coreutils}/bin/basename";
+    in ''
       : ''${XDG_CACHE_HOME:=$HOME/.cache}
       declare -A direnv_layout_dirs
       direnv_layout_dir () {
         echo "''${direnv_layout_dirs[$PWD]:=$(
-          local hash=$(echo $PWD | shasum | cut -c-8)
-          echo "$XDG_CACHE_HOME/direnv/layouts/$(basename $PWD)-$hash"
+          local hash=$(echo $PWD | ${sha256sum} | ${cut} -c-8)
+          echo "$XDG_CACHE_HOME/direnv/layouts/$(${basename} $PWD)-$hash"
         )}"
       }
     '';
