3e2ddd1a30bc425f4ba20dacc2e1272bdb53cc0e
grimerssy <grimerssy@gmail.com>
2023-05-10 13:35:23 +0200

    make executable shortcut in nvim global->user

--- home/programs/neovim/config/remap.lua
+++ home/programs/neovim/config/remap.lua
@@ -15,7 +15,7 @@ n("<C-k>", "cprev<CR>zz")
 n("<leader>n", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 -- to not overlap with lsp remaps
 n("<leader>rn", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
-n("<leader>x", "<CMD>!chmod +x %<CR>")
+n("<leader>x", "<CMD>!chmod u+x %<CR>")
 
 i("jk", "<ESC>")
 
