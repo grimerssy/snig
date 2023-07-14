9b3e18d74389e17cbde74153e7b2e43092af0f85
grimerssy <grimerssy@gmail.com>
2023-07-14 19:22:57 +0200

    fiddle with copilot settings in nvim

--- home/programs/neovim/plugins/copilot.nix
+++ home/programs/neovim/plugins/copilot.nix
@@ -2,6 +2,24 @@
 with pkgs; {
   programs.neovim = {
     extraPackages = [ nodejs ];
-    plugins = with vimPlugins; [ copilot-vim ];
+    plugins = with vimPlugins; [{
+      plugin = copilot-vim;
+      type = "lua";
+      config = ''
+        local n = require("config.keymap").nnoremap
+
+        vim.g.copilot_enabled = false
+
+        n("<leader>ce", function()
+          vim.cmd("Copilot enable")
+          vim.notify("Copilot enabled")
+        end)
+
+        n("<leader>cd", function()
+          vim.cmd("Copilot disable")
+          vim.notify("Copilot disabled")
+        end)
+      '';
+    }];
   };
 }
