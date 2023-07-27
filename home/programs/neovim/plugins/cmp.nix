08be657382eb6107b034605cf4d241b757da61df
grimerssy <grimerssy@gmail.com>
2023-07-27 11:16:56 +0200

    fix luasnip tab completion

--- home/programs/neovim/plugins/cmp.nix
+++ home/programs/neovim/plugins/cmp.nix
@@ -82,6 +82,16 @@
               "i",
               "s",
             }),
+            ["<Tab>"] = cmp.mapping(function(fallback)
+              if luasnip.jumpable(1) then
+                luasnip.jump(1)
+              else
+                fallback()
+              end
+            end, {
+              "i",
+              "s",
+            }),
             ["<S-Tab>"] = cmp.mapping(function(fallback)
               if luasnip.jumpable(-1) then
                 luasnip.jump(-1)
