24a1d967c933d2461a97838e6debdfcb57ec989b
Stanislav Stoyanov <grimerssy@gmail.com>
2023-11-11 14:18:24 +0100

    sign git commits and tags by default

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -1,32 +1,36 @@
 { ... }: {
   programs.git = {
     enable = true;
-    userName = "grimerssy";
+    userName = "Stanislav Stoyanov";
     userEmail = "grimerssy@gmail.com";
     delta.enable = true;
     ignores = [ "**/.direnv/" "**/.DS_Store" ];
+    signing = {
+      key = null;
+      signByDefault = true;
+    };
+    aliases = {
+      s = "status";
+      c = "commit";
+      ca = "commit --amend";
+      p = "push";
+      pf = "push --force";
+      co = "checkout";
+      l = "log --oneline --graph";
+      b = "branch";
+      m = "merge";
+      rb = "rebase";
+      rbi = "rebase -i";
+      rbc = "rebase --continue";
+      rs = "reset";
+      rh = "reset --hard";
+      rv = "revert";
+    };
     extraConfig = {
       core.editor = "nvim";
       pull = { ff = "only"; };
       init.defaultBranch = "main";
       format.pretty = "oneline";
-      alias = {
-        s = "status";
-        c = "commit";
-        ca = "commit --amend";
-        p = "push";
-        pf = "push --force";
-        co = "checkout";
-        l = "log --oneline --graph";
-        b = "branch";
-        m = "merge";
-        rb = "rebase";
-        rbi = "rebase -i";
-        rbc = "rebase --continue";
-        rs = "reset";
-        rh = "reset --hard";
-        rv = "revert";
-      };
     };
   };
 }
