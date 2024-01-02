1f0da1e6e028644d54ab2d49ec53bc58da863092
Stanislav Stoyanov <grimerssy@gmail.com>
2024-01-02 18:07:10 +0100

    fix typo

--- home/programs/neovim/plugins/notify.nix
+++ home/programs/neovim/plugins/notify.nix
@@ -2,6 +2,6 @@
   programs.neovim.plugins = with pkgs.vimPlugins; [{
     plugin = nvim-notify;
     type = "lua";
-    config = builtins.readFile ./nofify.lua;
+    config = builtins.readFile ./notify.lua;
   }];
 }
