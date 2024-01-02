1a7eaebae2f213cdbaa68eb3fbea17c3a2f2bb5a
Stanislav Stoyanov <grimerssy@gmail.com>
2024-01-02 19:39:19 +0100

    install devcontainer

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -1,6 +1,7 @@
 { ... }: {
   homebrew = {
     enable = true;
+    brews = [ "devcontainer" ];
     casks = [ "pika" "alex313031-thorium" "telegram" "discord" "insomnia" ];
     onActivation.cleanup = "zap";
   };
