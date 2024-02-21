b5db45877f5551e4dd4313d578255c4c915cce69
Stanislav Stoyanov <grimerssy@gmail.com>
2024-02-21 23:27:01 +0100

    remove unused nvim remaps

--- home/programs/neovim/configs/copilot.lua
+++ home/programs/neovim/configs/copilot.lua
@@ -1,18 +1,4 @@
-local copilot = require("copilot")
-
-local map = vim.keymap.set
-
-map("n", "<leader>ce", function()
-  vim.cmd("Copilot enable")
-  vim.notify("Copilot enabled")
-end)
-
-map("n", "<leader>cd", function()
-  vim.cmd("Copilot disable")
-  vim.notify("Copilot disabled")
-end)
-
-copilot.setup({
+require("copilot").setup({
   panel = { enabled = false },
   suggestion = {
     auto_trigger = true,
