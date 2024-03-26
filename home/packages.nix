64a6906dea0e2612131bf0d1bed16d92d2ddabc8
Stanislav Stoyanov <grimerssy@gmail.com>
2024-03-26 21:31:21 +0100

    remove unused stuff

--- home/packages.nix
+++ home/packages.nix
@@ -8,16 +8,6 @@
     ffmpeg
     du-dust
     ripgrep
-    cpulimit
-    parallel
-    watchexec
-    hyperfine
-    vhs
-    pop
-    gum
-    glow
-    flyctl
-    tectonic
     nix-index
     (uutils-coreutils.override { prefix = ""; })
   ];
