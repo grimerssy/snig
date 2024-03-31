d364ce3f171cad764a4eefeb9b02219e5241f855
Stanislav Stoyanov <grimerssy@gmail.com>
2024-03-31 16:06:08 +0200

    remove copilot from neovim

--- home/programs/neovim/configs/init.lua
+++ home/programs/neovim/configs/init.lua
@@ -7,7 +7,6 @@ local configs = {
   "lsp",
   "dap",
   "luasnip",
-  "copilot",
 
   "telescope",
   "treesitter",
--- home/programs/neovim/packages/copilot.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, ... }: {
-  programs.neovim = with pkgs; {
-    extraPackages = [ nodejs ];
-    plugins = with vimPlugins; [ copilot-lua ];
-  };
-}
