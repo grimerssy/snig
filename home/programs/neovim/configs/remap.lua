8165c1c7c2bd0518b5be652ef3993faecd859ad6
Stanislav Stoyanov <grimerssy@gmail.com>
2024-04-01 21:38:02 +0200

    lint remaps

--- home/programs/neovim/configs/remap.lua
+++ home/programs/neovim/configs/remap.lua
@@ -1,12 +1,12 @@
 local map = vim.keymap.set
 
-map({ "i" }, "jk", [[<ESC>]])
+map("i", "jk", [[<ESC>]])
 
 map("n", "<C-r>", [[:redo<CR>]])
 map("n", "<C-d>", [[<C-d>zzzv]])
 map("n", "<C-u>", [[<C-u>zzzv]])
-map("n", "n", [[nzzzv]], { silent = true })
-map("n", "N", [[Nzzzv]], { silent = true })
+map("n", "n", [[nzzzv]])
+map("n", "N", [[Nzzzv]])
 map("n", "<C-j>", [[cnext<CR>zz]])
 map("n", "<C-k>", [[cprev<CR>zz]])
 map("n", "<leader>rn", [[:%s/\<<C-r><C-w>\>//gI<Left><Left><Left>]])
