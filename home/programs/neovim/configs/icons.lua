468423d18f201ae9a2f14fdd6a619901039896cd
Stanislav Stoyanov <grimerssy@gmail.com>
2024-04-20 22:30:55 +0200

    add filetype icons

--- home/programs/neovim/configs/icons.lua
+++ home/programs/neovim/configs/icons.lua
@@ -1,12 +1,22 @@
 require("nvim-web-devicons").setup({
   strict = true,
   override_by_extension = {
-    ["astro"] = {
+    astro = {
       icon = "",
       color = "#ff5d01",
       name = "Astro",
     },
-    ["tape"] = {
+    gleam = {
+      icon = "",
+      color = "#f3b2ef",
+      name = "Gleam",
+    },
+    hcl = {
+      icon = "󱁢",
+      color = "#7345b6",
+      name = "Terraform",
+    },
+    tape = {
       icon = "󰨜",
       color = "#6948f6",
       name = "VHS",
