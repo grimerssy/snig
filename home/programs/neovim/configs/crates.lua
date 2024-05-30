88a8399d2cf1972aa311287f0188a567930ea72d
Stanislav Stoyanov <grimerssy@gmail.com>
2024-05-30 21:19:49 +0200

    update crates.nvim remaps

--- home/programs/neovim/configs/crates.lua
+++ home/programs/neovim/configs/crates.lua
@@ -15,29 +15,29 @@ vim.api.nvim_create_autocmd("BufRead", {
     end
 
     local opts = { buffer = true }
-    map("n", "<leader>rs", [[:s/{.*version.*=.*\("[0-9\.]\+"\).*}/\1<CR>]], opts)
+    map("n", "<leader>s", [[:s/{.*version.*=.*\("[0-9\.]\+"\).*}/\1<CR>]], opts)
 
-    map("n", "<leader>rt", crates.toggle, opts)
-    map("n", "<leader>rr", crates.reload, opts)
+    map("n", "<leader>T", crates.toggle, opts)
+    map("n", "<leader>R", crates.reload, opts)
 
-    map("n", "<leader>rv", double(crates.show_versions_popup), opts)
-    map("n", "<leader>rf", double(crates.show_features_popup), opts)
-    map("n", "<leader>rd", double(crates.show_dependencies_popup), opts)
+    map("n", "fv", double(crates.show_versions_popup), opts)
+    map("n", "ff", double(crates.show_features_popup), opts)
+    map("n", "fd", double(crates.show_dependencies_popup), opts)
 
-    map("n", "<leader>ru", crates.update_crate, opts)
-    map("n", "<leader>ra", crates.update_all_crates, opts)
-    map("n", "<leader>rU", crates.upgrade_crate, opts)
-    map("n", "<leader>rA", crates.upgrade_all_crates, opts)
+    map("n", "<leader>u", crates.update_crate, opts)
+    map("n", "<leader>a", crates.update_all_crates, opts)
+    map("n", "<leader>U", crates.upgrade_crate, opts)
+    map("n", "<leader>A", crates.upgrade_all_crates, opts)
 
-    map("v", "<leader>ru", crates.update_crates, opts)
-    map("v", "<leader>rU", crates.upgrade_crates, opts)
+    map("v", "<leader>u", crates.update_crates, opts)
+    map("v", "<leader>U", crates.upgrade_crates, opts)
 
-    map("n", "<leader>rx", crates.expand_plain_crate_to_inline_table, opts)
-    map("n", "<leader>rX", crates.extract_crate_into_table, opts)
+    map("n", "<leader>x", crates.extract_crate_into_table, opts)
+    map("n", "<leader>t", crates.expand_plain_crate_to_inline_table, opts)
 
-    map("n", "<leader>rH", crates.open_homepage, opts)
-    map("n", "<leader>rR", crates.open_repository, opts)
-    map("n", "<leader>rD", crates.open_documentation, opts)
-    map("n", "<leader>rC", crates.open_crates_io, opts)
+    map("n", "<leader>h", crates.open_homepage, opts)
+    map("n", "<leader>r", crates.open_repository, opts)
+    map("n", "<leader>d", crates.open_documentation, opts)
+    map("n", "<leader>c", crates.open_crates_io, opts)
   end,
 })
