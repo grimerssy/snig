5496e7894c988e55bb33e8a8f78992331b8d4c4c
Stanislav Stoyanov <grimerssy@gmail.com>
2024-11-12 16:27:05 +0100

    reformat nix args

--- system/shell.nix
+++ system/shell.nix
@@ -1,8 +1,4 @@
-{ user
-, lib
-, pkgs
-, ...
-}: {
+{ user, lib, pkgs, ... }: {
   system.activationScripts.postActivation.text = ''
     sudo chsh -s ${lib.getBin pkgs.fish}/bin/fish ${user}
   '';
