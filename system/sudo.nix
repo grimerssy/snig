e08dcaec106172c4e44d521817736920b22c05b4
Stanislav Stoyanov <grimerssy@gmail.com>
2025-01-17 20:13:19 +0100

    fix touch id auth in tmux

--- system/sudo.nix
+++ system/sudo.nix
@@ -1,3 +1,11 @@
 { ... }: {
-  security.pam.enableSudoTouchIdAuth = true;
+  # TODO https://github.com/LnL7/nix-darwin/pull/1020
+  disabledModules = [ "security/pam.nix" ];
+  imports = [
+    ../modules/security/pam.nix
+  ];
+  security.pam = {
+    enableSudoTouchIdAuth = true;
+    enablePamReattach = true;
+  };
 }
