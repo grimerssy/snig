75aac6637eb933085b6526fb9abeaa9d88354608
Stanislav Stoyanov <grimerssy@gmail.com>
2025-03-09 21:04:29 +0100

    inherit system specialArgs in home-manager modules

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -1,6 +1,12 @@
-{ ... }:
+{
+  inputs,
+  nixpkgs-stable,
+  nixpkgs-unstable,
+  ...
+}:
 {
   home-manager = {
+    extraSpecialArgs = { inherit inputs nixpkgs-stable nixpkgs-unstable; };
     sharedModules = [ ../home ];
     useGlobalPkgs = true;
   };
