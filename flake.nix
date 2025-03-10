43b08094d4c68d057592cce654636204fa86a7f3
Stanislav Stoyanov <grimerssy@gmail.com>
2025-03-10 18:36:21 +0100

    add treefmt

--- flake.nix
+++ flake.nix
@@ -1,22 +1,47 @@
 {
   inputs = {
+    nixpkgs.follows = "nixpkgs-stable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/release-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
 
     nix-darwin.url = "github:lnl7/nix-darwin/nix-darwin-24.11";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
 
-    nixpkgs.follows = "nixpkgs-stable";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = inputs: {
-    darwinConfigurations.hrk = inputs.nix-darwin.lib.darwinSystem {
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./modules/darwin
-        ./system/hrk.nix
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
       ];
+      imports = [ inputs.treefmt-nix.flakeModule ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.nixfmt.enable = true;
+            programs.stylua.enable = true;
+          };
+        };
+      flake = {
+        darwinConfigurations.hrk = inputs.nix-darwin.lib.darwinSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./modules/darwin
+            ./system/hrk.nix
+          ];
+        };
+      };
     };
-  };
 }
