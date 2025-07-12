44a92bb787eda64c053e59a270199719a7ddb616
Stanislav Stoyanov <grimerssy@gmail.com>
2025-07-12 10:57:52 +0200

    separate general modules from personal settings

--- modules/flake/configurations.nix
+++ modules/flake/configurations.nix
@@ -1,19 +1,70 @@
 { lib, inputs, ... }:
-let
-  darwinHosts = [
-    "hrk"
-  ];
-  mkDarwin =
-    host:
-    inputs.nix-darwin.lib.darwinSystem {
-      specialArgs = { inherit inputs; };
-      modules = [
-        ../darwin
-        { networking.hostName = lib.mkDefault host; }
-        ../../hosts/${host}.nix
-      ];
-    };
-in
 {
-  flake.darwinConfigurations = lib.genAttrs darwinHosts mkDarwin;
+  flake.darwinConfigurations.hrk = inputs.nix-darwin.lib.darwinSystem {
+    specialArgs = { inherit inputs; };
+    modules = [
+      inputs.mac-app-util.darwinModules.default
+
+      ../darwin
+
+      {
+        system.stateVersion = 4;
+        nixpkgs.hostPlatform = lib.systems.examples.aarch64-darwin;
+        imports = [ ../darwin/configuration ];
+        networking.hostName = lib.mkDefault "hrk";
+      }
+
+      inputs.home-manager.darwinModules.home-manager
+      {
+        config.users.users.grimerssy = {
+          name = "grimerssy";
+          home = "/Users/grimerssy";
+          createHome = true;
+          isHidden = false;
+        };
+        config.home-manager = {
+          extraSpecialArgs = { inherit inputs; };
+          useGlobalPkgs = true;
+          sharedModules = [
+            inputs.mac-app-util.homeManagerModules.default
+            ../home
+          ];
+          users.grimerssy.imports = [ ../../users/grimerssy.nix ];
+        };
+      }
+
+      {
+        nixpkgs.config.allowUnfree = true;
+        nix =
+          let
+            mapFlakes =
+              f:
+              lib.mapAttrs f {
+                inherit (inputs)
+                  nixpkgs
+                  nixpkgs-stable
+                  nixpkgs-unstable
+                  flakes
+                  ;
+                system = inputs.self;
+              };
+
+          in
+          {
+            enable = true;
+            optimise.automatic = true;
+            registry = mapFlakes (_: flake: { inherit flake; });
+            nixPath = mapFlakes (_: x: x);
+            settings = {
+              flake-registry = "";
+              experimental-features = "nix-command flakes";
+            };
+            gc = {
+              automatic = true;
+              options = "--delete-older-than 30d";
+            };
+          };
+      }
+    ];
+  };
 }
--- modules/home/configuration/gui/alacritty.nix
+++ modules/home/configuration/gui/alacritty.nix
@@ -1,8 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.alacritty = {
     enable = true;
     settings = {
+      terminal.shell.program = "${pkgs.fish}/bin/fish";
       cursor.style.blinking = "Never";
       font = {
         size = 17;
