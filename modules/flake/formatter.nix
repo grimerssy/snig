19fb8fe0c46954c0d7e21b2bb1bb8dd1a11cf07e
Stanislav Stoyanov <grimerssy@gmail.com>
2025-07-22 23:16:54 +0200

    add keep-sorted

--- modules/flake/formatter.nix
+++ modules/flake/formatter.nix
@@ -7,6 +7,7 @@
     projectRootFile = "flake.nix";
     programs.nixfmt.enable = true;
     programs.stylua.enable = true;
+    programs.keep-sorted.enable = true;
     settings.global.excludes = [
       ".editorconfig"
       "*.png"
