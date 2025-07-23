79217adb1f21d7de46085502dd3c4c97f7f0a6c7
Stanislav Stoyanov <grimerssy@gmail.com>
2025-07-23 15:00:28 +0200

    rename config directories for consistency

--- modules/flake/exports.nix
+++ modules/flake/exports.nix
@@ -3,11 +3,11 @@
   flake = {
     darwinModules = {
       default.imports = import ../darwin/module-list.nix;
-      configurations = import ../darwin/configuration/modules.nix;
+      configurations = import ../darwin/configurations/modules.nix;
     };
     homeManagerModules = {
       default.imports = import ../home/module-list.nix;
-      configurations = import ../home/configuration/modules.nix;
+      configurations = import ../home/configurations/modules.nix;
     };
   };
 }
